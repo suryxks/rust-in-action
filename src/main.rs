8243a0f82d1d1aa423a01b50562d27c0afe474a7
suryxks <suryasuresh9876543@gmail.com>
2024-02-01 11:28:23 +0530

    generics

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::convert::TryInto;
-
 use num::Complex;
+use std::time::{ Duration, Instant };
+use std::ops::{ Add };
 fn main() {
     let a = 10;
     let b: i32 = 20;
@@ -54,9 +55,66 @@ fn main() {
     let b = Complex::new(11.1, 22.2);
     let complex_result = a + b;
 
-    println!("{} + {}i", complex_result.re, complex_result.im)
+    println!("{} + {}i", complex_result.re, complex_result.im);
+
+    //flow control
+
+    let mut count = 0;
+    let time_limit = Duration::new(1, 0);
+    let start = Instant::now();
+
+    while Instant::now() - start < time_limit {
+        count += 1;
+    }
+
+    println!("{}", count);
+
+    let haystack = [1, 1, 2, 5, 14, 42, 132, 429, 1430, 4862];
+
+    for item in &haystack {
+        let result = match item {
+            42 | 132 => "hit!",
+            _ => "miss",
+        };
+        if result == "hit!" {
+            println!("{}:{}", item, result);
+        }
+    }
+
+    let a = 42;
+    let r = &a;
+    let b = a - *r;
+
+    println!("a-a={}", b);
+
+    let needle = 0o204;
+
+    for item in &haystack {
+        if *item == needle {
+            println!("{}", item);
+        }
+    }
+
+    let res = add_with_lifetimes(&a, &b);
+    println!("{}", res);
+
+    let floats = add_generic(1.2, 1.4);
+    let ints = add_generic(a, b);
+    let durations = add_generic(Duration::new(5, 0), Duration::new(6, 0));
+
+    println!("{}", floats);
+    println!("{}", ints);
+    println!("{:?}", durations);
 }
 
 fn add(i: i32, j: i32) -> i32 {
     i + j
+}
+
+fn add_with_lifetimes<'a, 'b>(i: &'a i32, j: &'b i32) -> i32 {
+    *i + *j
+}
+
+fn add_generic<T: Add<Output = T>>(a: T, b: T) -> T {
+    a + b
 }
